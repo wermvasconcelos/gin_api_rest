88929f5fcf3f54147d29bdc65f7a2521c3f4cefd
wermvasconcelos <wermenson.vasconcelo@gmail.com>
2024-01-19 14:19:48 -0300

    feat: rota alunos

--- main.go
+++ main.go
@@ -4,5 +4,13 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+	r.GET("/alunos", ExibeTodosOsAlunos)
 	r.Run()
+}
+
+func ExibeTodosOsAlunos(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"nome": "Werm",
+	})
 }
