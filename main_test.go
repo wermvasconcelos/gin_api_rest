9931e9e55a14a652f546a0339be8e66bb5e9c7c3
wermvasconcelos <wermenson.vasconcelo@gmail.com>
2024-01-22 15:18:15 -0300

    feat: teste route /alunos

--- main_test.go
+++ main_test.go
@@ -9,11 +9,26 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"github.com/wermvasconcelos/api_go_gin/controllers"
+	"github.com/wermvasconcelos/api_go_gin/database"
+	"github.com/wermvasconcelos/api_go_gin/models"
 )
+
+var ID int
 
 func SetupDasRotasDeTeste() *gin.Engine {
 	rotas := gin.Default()
 	return rotas
+}
+
+func CriaAlunoMock() {
+	aluno := models.Aluno{Nome: "Aluno Teste", CPF: "12345678911", RG: "123456789"}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+
+func DeletaAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
 }
 
 func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
@@ -29,4 +44,18 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	mockDaResposta := `{"API diz:":"E ai werm, Tudo belez?"}`
 	respostaBody, _ := ioutil.ReadAll(resposta.Body)
 	assert.Equal(t, mockDaResposta, string(respostaBody))
+}
+
+func TestListandoTodosOsAlunosHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.ExibeTodosOsAlunos)
+
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
 }
